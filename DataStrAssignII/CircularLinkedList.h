5ccc65df879bd911c3ada6a56d598047ce9d1ccc
Omar Mohammad <omarmohamed75689@gmail.com>
2023-04-24 00:28:17 +0200

    Circular Linked List Class and prototypes

--- DataStrAssignII/CircularLinkedList.h
+++ DataStrAssignII/CircularLinkedList.h
@@ -1,1 +1,32 @@
-#pragma once
+#include <iostream>
+using namespace std;
+
+struct NodeType {
+	Type info;
+	NodeType<Type>* next;
+};
+template<typename Type>
+class SingleLinkedList {
+private:
+	NodeType<Type>* first;
+	NodeType<Type>* last;
+	int count = 0;
+public:
+	
+	void insertAtHead(Type element);
+	void insertAtTail(Type element);
+	void insertAt(Type element, int index);
+	void removeAtHead();
+	void removeAtTail();
+	void removeAt(int index);
+	void retrieveAt(int index);
+	void replaceAt(Type newElement, int index);
+	bool isExist(Type element);
+	bool isItemAtEqual(Type element, int index);
+	void swap(int index1, int index2);
+	bool isEmpty();
+	int linkedListSize();
+	void clear();
+	void print();
+
+};
