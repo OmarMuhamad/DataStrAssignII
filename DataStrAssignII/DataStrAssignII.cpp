ae0a0a8adb3f4f5a3079ab1d4f3f01f331cdcb81
Omar Mohammad <omarmohamed75689@gmail.com>
2023-04-23 22:32:29 +0200

    main updated

--- DataStrAssignII/DataStrAssignII.cpp
+++ DataStrAssignII/DataStrAssignII.cpp
@@ -1,21 +1,20 @@
 // Author OmarMohammad
 // Last update 4/7/2023
 
-//#include "ArrayBasedList.h"
-#include "SingleLinkedList.h"
+#include "ArrayBasedList.h"
+//#include "SingleLinkedList.h"
 //#include "StackLinkedList.h"
+#include "QueueLinkedList.h"
 int main() {
-	SingleLinkedList<int> lst;
-	lst.insertAtTail(1);
-	lst.insertAtTail(2);
-	lst.insertAtTail(3);
-	lst.insertAtTail(4);
-	lst.insertAtTail(5);
-	//lst.replaceAt(8, 2);
-	cout << lst.linkedListSize() << endl;
-	lst.swap(1, 2);
-	lst.print();
-	//lst.clear();
-	//cout << lst.linkedListSize() << endl;
-	//lst.retrieveAt(1);
+	Queue<int> qu;
+	qu.enqueue(1);
+	qu.enqueue(2);
+	qu.enqueue(3);
+	qu.enqueue(4);
+	qu.enqueue(5);
+	cout << qu.dequeuee() << endl;
+	qu.print();
+	cout << qu.queueSize() << endl;
+	cout << qu.First() << endl;
+
 }
