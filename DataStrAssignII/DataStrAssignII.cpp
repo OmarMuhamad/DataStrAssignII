67b3f462a6610531810560f61218d080501e5c55
Omar Mohammad <omarmohamed75689@gmail.com>
2023-04-24 19:34:37 +0200

    added generate binary number and updated enqueue operation

--- DataStrAssignII/DataStrAssignII.cpp
+++ DataStrAssignII/DataStrAssignII.cpp
@@ -3,10 +3,9 @@
 
 //#include "ArrayBasedList.h"
 //#include "SingleLinkedList.h"
-#include "StackLinkedList.h"
-//#include "QueueLinkedList.h"
+//#include "StackLinkedList.h"
+#include "QueueLinkedList.h"
 int main() {
-	Stack<char> st;
-	cout << st.infixToPostfix("a*b+c");
+	generateBinaryNum(10);
 
 }
