7374e72a521d5f2e5c05947e5808854ea6c6a247
Omar Mohammad <omarmohamed75689@gmail.com>
2023-04-24 17:49:19 +0200

    infix2postfix is added

--- DataStrAssignII/DataStrAssignII.cpp
+++ DataStrAssignII/DataStrAssignII.cpp
@@ -1,20 +1,12 @@
 // Author OmarMohammad
 // Last update 4/7/2023
 
-#include "ArrayBasedList.h"
+//#include "ArrayBasedList.h"
 //#include "SingleLinkedList.h"
-//#include "StackLinkedList.h"
-#include "QueueLinkedList.h"
+#include "StackLinkedList.h"
+//#include "QueueLinkedList.h"
 int main() {
-	Queue<int> qu;
-	qu.enqueue(1);
-	qu.enqueue(2);
-	qu.enqueue(3);
-	qu.enqueue(4);
-	qu.enqueue(5);
-	cout << qu.dequeuee() << endl;
-	qu.print();
-	cout << qu.queueSize() << endl;
-	cout << qu.First() << endl;
+	Stack<char> st;
+	cout << st.infixToPostfix("a*b+c");
 
 }
