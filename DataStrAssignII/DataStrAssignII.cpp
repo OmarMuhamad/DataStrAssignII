fcdf5c97732da8946c0cec7fcb7b782fc295cf75
Omar Mohammad <omarmohamed75689@gmail.com>
2023-04-25 15:19:32 +0200

    add two test cases to test combine all  nodes

--- DataStrAssignII/DataStrAssignII.cpp
+++ DataStrAssignII/DataStrAssignII.cpp
@@ -2,10 +2,31 @@
 // Last update 24/7/2023
 
 //#include "ArrayBasedList.h"
-//#include "SingleLinkedList.h"
-#include "StackLinkedList.h"
+#include "SingleLinkedList.h"
+//#include "StackLinkedList.h"
 //#include "QueueLinkedList.h"
 int main() {
-	cout << infixToPostfix("X^Y/(5*Z)+2");
+	SingleLinkedList<int> ll;
+	cout << "tc: 1" << endl;
+	ll.insertAtTail(0);
+	ll.insertAtTail(1);
+	ll.insertAtTail(0);
+	ll.insertAtTail(3);
+	ll.insertAtTail(0);
+	ll.insertAtTail(2);
+	ll.insertAtTail(2);
+	ll.insertAtTail(0);
+	CombineNodes(ll.returnFirst());
+	ll.clear();
+	cout << "tc: 2" << endl;
+	ll.insertAtTail(0);
+	ll.insertAtTail(3);
+	ll.insertAtTail(1);
+	ll.insertAtTail(0);
+	ll.insertAtTail(4);
+	ll.insertAtTail(5);
+	ll.insertAtTail(2);
+	ll.insertAtTail(0);
+	CombineNodes(ll.returnFirst());
 
 }
